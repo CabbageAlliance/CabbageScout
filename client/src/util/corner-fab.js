bd3014e3b4594fd0aa38bf3ba16ad6cf2b82c89c
Jonah Snider <jonah@jonah.pw>
2020-02-14 20:14:27 -0800

    perf(client): remove unecessary createStyles call
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- client/src/util/corner-fab.js
+++ client/src/util/corner-fab.js
@@ -1,6 +1,6 @@
-import {createStyles, makeStyles} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
 
 /**
  * Material UI styles for a Floating Action Button (FAB) that is positioned in the corner of the screen.
  */
-export const cornerFabStyles = makeStyles(theme => createStyles({fab: {position: 'absolute', bottom: theme.spacing(2), right: theme.spacing(2)}}));
+export const cornerFabStyles = makeStyles(theme => ({fab: {position: 'absolute', bottom: theme.spacing(2), right: theme.spacing(2)}}));
