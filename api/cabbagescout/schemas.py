ef42d0158660b8e1a346e5ef954ee386b6a7ece6
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-02-20 12:54:04 -0800

    refactor(api): flatten ScoutEntry schema (WIP)

--- api/cabbagescout/schemas.py
+++ api/cabbagescout/schemas.py
@@ -1,71 +1,95 @@
 from datetime import timedelta
-from typing import Dict, List
+from typing import Dict, List, NamedTuple
 
-from pydantic import BaseModel, Field
-
-
-class PowerCells(BaseModel):
-    # The number of power cells scored in the upper goal
-    # note: Not the total score from upper goal shots
-    # note: Includes both inner and outer goal
-    uppergoal_score: int = Field(..., ge=0)
-    # The number of power cells scored in the lower goal
-    lowergoal_score: int = Field(..., ge=0)
-    # The number of shots to the upper goal that missed (did not give any points)
-    uppergoal_missed: int = Field(..., ge=0)
+from pydantic import BaseModel, Field, constr
 
 
-class ControlPanel(BaseModel):
+class ScoutEntry(BaseModel):
+    match: int = Field(
+        ..., ge=1, description="The match number of a single event of qualifiers"
+    )
+    team: int = Field(
+        ..., ge=1, le=9999, description="The team number of the scouted team"
+    )
+
+    # Auto
+
+    auto_crossed_line: bool = Field(
+        ...,
+        description="Did the robot exit the initiation line during the autonomous period?",
+    )
+    auto_uppergoal_score: int = Field(
+        ...,
+        ge=0,
+        description="Total number of power cells scored in upper goal during autonomous period, both outer and inner",
+    )
+    auto_lowergoal_score: int = Field(
+        ...,
+        ge=0,
+        description="Number of power cells scored in lower goal during autonomous period",
+    )
+    auto_uppergoal_missed: int = Field(
+        ...,
+        ge=0,
+        description="Number of shots during autonomous period to the upper goal that missed (did not give any points)",
+    )
+
+    # Teleop
+
+    teleop_uppergoal_score: int = Field(
+        ...,
+        ge=0,
+        description="Total number of power cells scored in upper goal during teleoperated period, both outer and inner",
+    )
+    teleop_lowergoal_score: int = Field(
+        ...,
+        ge=0,
+        description="Number of power cells scored in lower goal during teleoperated period",
+    )
+    teleop_uppergoal_missed: int = Field(
+        ...,
+        ge=0,
+        description="Number of shots during teleoperated period to the upper goal that missed (did not give any points)",
+    )
+
+    # # The control panel data for a team during the tele-operated period
     # The amount of time spent rotating the control panel for ROTATION CONTROL
     rotation_control_time: timedelta
     # The amount of time spent rotating the control panel for POSITION CONTROL
     position_control_time: timedelta
-
-
-class Auto(BaseModel):
-    # Exited the initiation line during the autonomous period
-    crossed_line: bool
-    # The power cell data for scoring during the autonomous period
-    power_cells: PowerCells
-
-
-class Teleop(BaseModel):
-    # The power cell data for scoring during the tele-operated period
-    power_cells: PowerCells
-    # The control panel data for a team during the tele-operated period
-    control_panel: ControlPanel
     # The amount of time spent playing defense on other teams (not actively scoring)
-    time_defending: timedelta
+    defending_time: timedelta
 
+    # # # Endgame
 
-class Endgame(BaseModel):
     # The team seemed to make an effort to perform the CLIMB task
-    climb_attempted: bool
+    hang_attempted: bool
     # The team was awarded points for their attempt at the CLIMB task
-    climb_succeeded: bool
+    hang_succeeded: bool
     # The amount of time spent performing the CLIMB task
-    climb_time: timedelta
+    hang_time: timedelta
     # The GENERATOR SWITCH was balanced at the end of the endgame period
-    climb_balanced: bool
+    hang_level: bool
 
+    # # # Other
 
-class ScoutEntry(BaseModel):
-    # The team number of the scouted team (int in [1, 9999])
-    team: int = Field(..., ge=1, le=9999)
-    # The autonomous data for that team during the match
-    auto: Auto
-    # The tele-operated data for that team during the match
-    teleop: Teleop
-    # The endgame data for that team during the match
-    endgame: Endgame
     # The "star rating" for the drivers' skills (int in [1, 5])
     driver_rating: int = Field(..., ge=1, le=5)
     # The amount of time a robot spent visibly disabled during a match
-    time_dead: timedelta
-    # Additional scouter comments
-    comments: str
+    down_time: timedelta
+    # Additional scouter comments (multiline)
+    comments: constr(strip_whitespace=True, max_length=512)
     # The team received a foul during the match
     received_foul: bool
+
+
+class ScoutEntryKey(NamedTuple):
+    @classmethod
+    def from_scoutentry(cls, entry: ScoutEntry):
+        return cls(match=entry.match, team=entry.team)
+
+    match: int
+    team: int
 
 
 class Database(BaseModel):  # lmao, this is temporary, chillax
