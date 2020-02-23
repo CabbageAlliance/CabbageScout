0dc46b7d4ebfae2c9c9922b7dfd83f340fa108eb
Kevin Wang <kev@kevwang.dev>
2020-02-22 22:49:40 -0800

    fix(client): make new entry submit actually work

--- client/src/pages/scout/new.jsx
+++ client/src/pages/scout/new.jsx
@@ -1,25 +1,40 @@
 import {Box, Button, TextField, Typography} from '@material-ui/core';
 import {useRouter} from 'next/router';
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Layout from '../../components/Layout';
+import ScoutEntryContext from '../../util/ScoutEntryContext';
 
-const inputSettings = {min: 1, step: 1, pattern: '^\\d+$'};
+const checkValidNumber = num => num && num > 0 && num % 1 === 0 && num < 10000;
 
 const New = () => {
 	const router = useRouter();
+	const {entryDispatch} = useContext(ScoutEntryContext);
 
 	const [match, setMatch] = useState('');
+	const [validMatch, setValidMatch] = useState(true);
 	const handleMatchChange = event => {
 		setMatch(event.target.value);
+		setValidMatch(checkValidNumber(event.target.value));
 	};
 
 	const [team, setTeam] = useState('');
+	const [validTeam, setValidTeam] = useState(true);
 	const handleTeamChange = event => {
 		setTeam(event.target.value);
+		setValidTeam(checkValidNumber(event.target.value));
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+	const [submitReady, setSubmitReady] = useState(false);
+	useEffect(() => setSubmitReady(match && validMatch && team && validTeam), [match, validMatch, team, validTeam]);
+
+	const handleSubmit = event => {
+		event.preventDefault()
+
+		entryDispatch({
+			type: 'reset',
+			data: {team, match}
+		});
+
 		router.push('/scout/scout');
 	};
 
@@ -29,32 +44,39 @@ const New = () => {
 				<Typography variant='h3'>New Entry</Typography>
 				<Box mt={2}>
 					<TextField
-						required
 						fullWidth
 						id='match'
 						variant='outlined'
 						label='Match'
-						type='number'
-						inputProps={inputSettings}
+						type="number"
 						value={match}
+						error={!validMatch}
+						onKeyUp={handleMatchChange} // This fixes some edge cases
 						onChange={handleMatchChange}
 					/>
 				</Box>
 				<Box mt={2}>
 					<TextField
-						required
 						fullWidth
 						id='team-number'
 						variant='outlined'
 						label='Team Number'
 						type='number'
-						inputProps={{...inputSettings, max: 9999}}
 						value={team}
+						error={!validTeam}
+						onKeyUp={handleTeamChange}
 						onChange={handleTeamChange}
 					/>
 				</Box>
 				<Box mt={2}>
-					<Button variant='contained' color='secondary' size='large' type='submit' onSubmit={handleSubmit}>
+					<Button
+						variant='contained'
+						color='secondary'
+						size='large'
+						type='submit'
+						disabled={!submitReady}
+						onClick={handleSubmit} // I would use onSubmit if it didn't do wack stuff
+					>
 						Start
 					</Button>
 				</Box>
