967325485b1d9a0ade3f51e0f7d1f936ee5572eb
Jonah Snider <jonah@jonah.pw>
2020-03-01 18:11:22 -0800

    refactor(client): rewrite downloading logic
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- client/src/pages/data/download.jsx
+++ client/src/pages/data/download.jsx
@@ -2,42 +2,46 @@ import React, {useState} from 'react';
 import Layout from '../../components/Layout';
 import {Button} from '@material-ui/core';
 import Alert from '../../components/Alert';
+import propTypes from 'prop-types';
+import download from 'js-file-download';
+
+const DownloadAnchor = props => <a href={props.href} download={props.download} />;
+
+DownloadAnchor.propTypes = {
+	href: propTypes.string.isRequired,
+	download: propTypes.string.isRequired
+};
 
 const Download = () => {
-	const [error, setError] = useState(false);
-	const handleToggleError = open => () => setError(open);
+	const [errorStatus, setErrorStatus] = useState(false);
+	const handleToggleError = open => () => setErrorStatus(open);
 
-	const handleDownload = () =>
-		fetch('/api/csv')
-			.then(resp => {
-				if (resp.ok) {
-					return resp.blob();
-				}
+	const downloadCSV = async () => {
+		let blob;
 
-				return Promise.reject(resp);
-			})
-			.then(blob => {
-				const url = window.URL.createObjectURL(blob);
-				const link = document.createElement('a');
-				link.href = url;
-				link.download = 'scout-data.csv';
-				document.body.append(link);
-				link.click();
-				link.remove();
-			})
-			.catch(error_ => {
-				console.log(error_);
-				setError(true);
-			});
+		try {
+			const res = await fetch(`${process.env.url}/api/csv`);
+			if (res.ok) {
+				blob = await res.blob();
+			} else {
+				throw res;
+			}
+		} catch (error) {
+			console.error(error);
+			setErrorStatus(true);
+		}
+
+		return download(blob, 'scout-data.csv', 'text/csv');
+	};
 
 	return (
 		<Layout title='Download'>
-			<Button variant='contained' color='secondary' size='large' onClick={handleDownload}>
+			<Button variant='contained' color='secondary' size='large' onClick={downloadCSV}>
 				Download All Data (CSV)
 			</Button>
 			<Alert
 				error
-				open={error}
+				open={errorStatus}
 				message={
 					process.env.netlify
 						? 'You are running a static version of the site. The API is not running, so all requests will fail.'
