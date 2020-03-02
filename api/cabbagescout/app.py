4d437bcb9c25700e0d82aa7fa9e19c4c2c8aef38
SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>
2020-03-01 22:50:36 -0800

    perf(api): add __slots__ to WebServer
    Signed-off-by: SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>

--- api/cabbagescout/app.py
+++ api/cabbagescout/app.py
@@ -5,6 +5,8 @@ from .api import Api
 
 
 class WebServer:
+    __slots__ = ("api", "app", "database")
+
     def __init__(self, database: Database):
         self.app = Starlette(
             debug=True, on_startup=[database.connect], on_shutdown=[database.close]
