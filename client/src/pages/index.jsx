9182e3acf765c6ac9b4c0105427a3716d4f5eb22
Jonah Snider <jonah@jonah.pw>
2020-03-03 17:16:00 -0800

    feat(client): add placeholder latest matches table
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React, {useState} from 'react';
 import Layout from '../components/Layout';
-import {Button} from '@material-ui/core';
+import {Button, TableBody, TableRow, TableCell, TableHead, Table, TableContainer, Paper, Box} from '@material-ui/core';
 import {useRouter} from 'next/router';
 
 import Alert from '../components/Alert';
@@ -11,6 +11,20 @@ const Home = () => {
 	const [snackbar, setSnackbar] = useState(Boolean(router.query.submit));
 	const handleSnackbarClose = () => setSnackbar(false);
 
+	/**
+	 * @typedef {MatchScoutListing}
+	 * @property {string} executor The user who scouted this match
+	 * @property {number} match The match number
+	 * @property {number} team The team being scouted
+	 */
+
+	/** @type {MatchScoutListing[]} */
+	const rows = [
+		{executor: 'Kev', match: 1, team: 199},
+		{executor: 'Jonah', match: 2, team: 581},
+		{executor: 'Sebby', match: 3, team: 253}
+	];
+
 	return (
 		<Layout title='Home'>
 			<Link passHref href='/scout/new'>
@@ -18,6 +32,32 @@ const Home = () => {
 					Start scouting
 				</Button>
 			</Link>
+			<Box mt={4}>
+				<TableContainer component={Paper}>
+					<Table aria-label='Latest scouted matches'>
+						<TableHead>
+							<TableRow>
+								<TableCell>Latest scouted matches</TableCell>
+								<TableCell align='right'>Match</TableCell>
+								<TableCell align='right'>Team</TableCell>
+								<TableCell align='right'>Executor</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{rows.map(row => (
+								<TableRow key={[row.team, row.match].join()}>
+									<TableCell component='th' scope='row'>
+										Match {row.match} team {row.team}
+									</TableCell>
+									<TableCell align='right'>{row.match}</TableCell>
+									<TableCell align='right'>{row.team}</TableCell>
+									<TableCell align='right'>{row.executor}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Box>
 
 			<Alert open={snackbar} message='Successfully recorded scouting entry!' onClose={handleSnackbarClose} />
 		</Layout>
